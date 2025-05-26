15a34aaa08d14b14154f896ecb4eb0d98a3fba52
santiagodiazs <sdiazsieiro@itba.edu.ar>
2025-05-25 23:22:27 -0300

    Fixing frontend deployment issues

--- autowhapp-backend/db.js
+++ autowhapp-backend/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.DB_USER || 'autowhapp_user',
-  host: process.env.DB_HOST || 'localhost',
-  database: process.env.DB_NAME || 'autowhapp',
+  user: process.env.DB_USER, /* || 'autowhapp_user', */
+  host: process.env.DB_HOST, /* || 'localhost', */
+  database: process.env.DB_NAME /* || 'autowhapp' */,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT || 5432
+  port: process.env.DB_PORT /* || 5432 */
 });
 
 pool.connect((err) => {
