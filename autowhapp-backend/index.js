2a8c1b04a530171f1e320ac0f90caed365dae665
santiagodiazs <sdiazsieiro@itba.edu.ar>
2025-07-13 20:02:17 -0300

    fixed envs

--- autowhapp-backend/index.js
+++ autowhapp-backend/index.js
@@ -43,8 +43,8 @@ initializeClients();
 // Middleware
 const corsOptions = {
   origin: [
-    process.env.FRONTEND_URL /* || 'http://localhost:3001',
-    'https://front-production-a39e.up.railway.app', */
+    process.env.FRONTEND_URL || 'http://localhost:3001',
+    'https://front-production-a39e.up.railway.app',
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
